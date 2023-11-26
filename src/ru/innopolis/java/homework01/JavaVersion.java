6ccfce923aa31c565b2079dc40b2645a4c7ad8b7
GreyDervish <150971759+GreyDervish@users.noreply.github.com>
2023-11-26 23:13:47 +0300

    add homework01

---
+++ src/ru/innopolis/java/homework01/JavaVersion.java
@@ -0,0 +1,8 @@
+package ru.innopolis.java.homework01;
+public class JavaVersion {
+        public static void main(String[] args) {
+            String javaversion = System.getProperty("java.version");
+            System.out.println("Версия java: " + javaversion);
+
+        }
+}
